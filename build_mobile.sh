44f4e03fc480a947b892cb3d5ad594a7e6d7273f
min7.choi <min7.choi@samsung.com>
2016-01-28 10:51:32 +0900

    modified build config - .gbs.conf, build script, xml, manifeset file
    Change-Id: Ia581bcc049d3849256d11bcc518d3e88ebb06922
    Signed-off-by: min7.choi <min7.choi@samsung.com>

--- build_mobile.sh
+++ build_mobile.sh
@@ -1,2 +1,2 @@
-gbs -c .gbs.conf build -P tzmo_v3.0_arm-wayland -A armv7l -D tzmb_v3.0_arm-wayland.conf --include-all $@
-#gbs -c .gbs.conf build -P tzmo_v3.0_arm-wayland -A armv7l --include-all $@
+gbs -c .gbs.conf build -P profile.tzmo_v3.0_arm-wayland -A armv7l --include-all
+#gbs -c .gbs.conf build -P profile.tzmo_v3.0_arm-wayland_mirror -A armv7l --include-all
